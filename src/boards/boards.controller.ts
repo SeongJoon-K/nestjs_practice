c718b1806d2f2c37b1bdac2248c7299d30250fa7
SEONGJOON KIM <jobcho6320@naver.com>
2022-12-18 14:55:10 +0900

    feat: 게시글 등록 POST API 구현

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Board } from './boards.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  @Get()
-  getAllBoard() {
-    return this.boardsService.getAllBoard;
+  @Get('/')
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
+  }
+
+  @Post('/')
+  createBoard(@Body('title') title: string, @Body('description') description: string): Board {
+    return this.boardsService.createBoard(title, description);
   }
 }
