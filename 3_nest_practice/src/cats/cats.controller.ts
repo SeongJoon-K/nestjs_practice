636a29d49da126b7baf34b345df9704812455b4a
SEONGJOON KIM <jobcho6320@naver.com>
2023-02-24 21:15:33 +0900

    http-exception filter, validation pipe

--- 3_nest_practice/src/cats/cats.controller.ts
+++ 3_nest_practice/src/cats/cats.controller.ts
@@ -1,16 +1,31 @@
+import { HttpExceptionFilter } from './../http-exeption.filter';
 import { CatsService } from './cats.service';
-import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseFilters,
+} from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
+  @UseFilters(HttpExceptionFilter)
   getAllCat() {
+    throw new HttpException('api is broken', 401);
     return 'all cat';
   }
   @Get(':id')
-  getOneCat() {
+  getOneCat(@Param('id', ParseIntPipe) param: number) {
+    console.log(param);
+
     return 'one cat';
   }
 
